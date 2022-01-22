f2dfc4367bbce43867f8bcd6ce4a5af9298519a1
SatriaMulyaa <satriaamulyaa@gmail.com>
2022-01-22 22:12:48 +0700

    type data number in php

---
+++ Part2-TypeDataNumber.php
@@ -0,0 +1,33 @@
+<?php
+echo "Decimal : ";
+var_dump(1234);
+
+echo "HexaDecimal : ";
+var_dump(0x1A);
+
+echo "Octal : ";
+var_dump(0121212);
+
+echo "Binar : ";
+var_dump(0b011010);
+
+echo "Underscore at number : ";
+var_dump(132_219);
+
+echo "Floating Point : ";
+var_dump(1.234);
+
+echo "Floating Point with E-notion : 1.2 x 1000";
+var_dump(1.2e3);
+
+echo "Floating Point with E-notion minus : 1.2 * 0.001";
+var_dump(1.2e-3);
+
+echo "Floating point uderscore : ";
+var_dump(1_343413.989);
+
+echo "integer overflow : ";
+var_dump(9223372036854775807);
+
+echo "integer overflow : ";
+var_dump(9223372036854775808);
