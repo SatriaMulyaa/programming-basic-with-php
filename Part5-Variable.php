2482f861399593ed7434a17beb8716c6a7bd523f
SatriaMulyaa <satriaamulyaa@gmail.com>
2022-01-23 17:20:43 +0700

    add variables content

--- Part5-Variable.php
+++ Part5-Variable.php
@@ -12,3 +12,14 @@ echo "\n";
 // we can change this age int to string, this is dynamic type
 $age = "dua puluh";
 echo $age;
+
+echo "\n\n===variable variables===\n";
+$contoh = "eko";
+$$contoh = "kurniawan";
+
+echo "contoh : ";
+echo $contoh;
+echo "\n";
+
+echo "name : ";
+echo $eko;
