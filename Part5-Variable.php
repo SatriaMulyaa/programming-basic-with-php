9cee32f50ad3d303dc45d292ded5dea2262b206b
SatriaMulyaa <satriaamulyaa@gmail.com>
2022-01-23 11:10:56 +0700

    added part5 variable

---
+++ Part5-Variable.php
@@ -0,0 +1,14 @@
+<?php
+$name = "Satria";
+echo "name : ";
+echo $name;
+
+echo "\n";
+
+$age = 20;
+echo $age;
+echo "\n";
+
+// we can change this age int to string, this is dynamic type
+$age = "dua puluh";
+echo $age;
